a5a2348cd7fd1bea9cbde9b301f99a325e3457cb
Melvin Gaviola <kujigaviola@gmail.com>
2018-06-14 12:38:19 +0800

    Provided .cpp file and the data library.txt in text form

---
+++ sorting.cpp
@@ -0,0 +1,32 @@
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define N 20
+
+//	Structure
+typedef struct node{
+	int book_no;
+	int page_no;
+	char title[255];
+	char ISBN[255];
+	char contents[255];
+	char author[255];
+}records[N];
+
+//	Prototype
+void readRec(records);
+void writeRec(records);
+void sort(records);
+void display(records);
+
+
+
+int main()
+{
+	records book_record;
+
+	return 0;
+}
+
+
